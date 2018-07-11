a14ee6d75b10ccb22c7cc59d954b1453e43e9c31
Charles Stover <charles.stover@walmart.com>
2018-07-11 16:17:53 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-portfolio
 A sleek portfolio design created in React.
+
+## Examples
+* [AceAlters.com](https://acealters.com/)
+* [CharlesStover.com](https://charlesstover.com/)
+* [Quisido.com](https://quisido.com/)
---
+++ index.js
@@ -0,0 +1,13 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
+
+export default class ReactPortfolio extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <CssBaseline />
+        Portfolio
+      </div>
+    );
+  }
+}
