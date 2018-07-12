a7e37478aff9b867b5b30082766f952e94a638ed
Charles Stover <charles.stover@walmart.com>
2018-07-12 12:12:37 -0500

    peer dependencies

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # react-portfolio
 A sleek portfolio design created in React.
 
+## Build
+`npm run build`
+
+## Install
+* `npm install react-portfolio --save` or
+* `yarn add react-portfolio`
+
 ## Examples
 * [AceAlters.com](https://acealters.com/)
 * [CharlesStover.com](https://charlesstover.com/)
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,24 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   externals: {
-    'react': 'commonjs react'
+    '@material-ui/core': {
+      amd: '@material-ui/core',
+      commonjs: '@material-ui/core',
+      commonjs2: '@material-ui/core'
+    },
+    'react': {
+      amd: 'react',
+      commonjs: 'react',
+      commonjs2: 'react',
+      root: 'React'
+    },
+    'react-router-dom': {
+      amd: 'react-router-dom',
+      commonjs: 'react-router-dom',
+      commonjs2: 'react-router-dom'
+    }
   },
+  mode: 'production',
   module: {
     rules: [
       {
@@ -26,7 +42,15 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
-    path: path.resolve(__dirname, 'build')
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'build'),
+    umdNamedDefine: true
+  },
+  resolve: {
+    alias: {
+      '@material-ui/core': path.resolve(__dirname, './node_modules/@material-ui/core'),
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-router-dom': path.resolve(__dirname, './node_modules/react-router-dom')
+    }
   }
 };
