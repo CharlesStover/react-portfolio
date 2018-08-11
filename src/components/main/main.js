98dfc378ad18606024ee2dcaa641bf5091b2663e
Charles Stover <github@charlesstover.com>
2018-08-11 16:43:45 -0500

    react-helmet

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,17 +1,31 @@
 import memoizeOne from 'memoize-one';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Context from '../../context';
 import withStyles from './main-styles';
 
 const mapRoutesToSwitch = routes =>
-  routes.map(props =>
+  routes.map(route =>
     <Route
       exact
+      path={route.path}
+      render={props => {
+        const Component = route.component;
+        return (
+          <React.Fragment>
+            <Helmet>
+              {route.description ? <meta name="description" value={route.description} /> : null}
+              {route.keywords ? <meta name="keywords" value={route.keywords.join(', ')} /> : null}
+              {route.title ? <title children={route.title} /> : null}
+            </Helmet>
+            <Component {...props} />
+          </React.Fragment>
+        );
+      }}
       sensitive
       strict
-      {...props}
     />
   );
 
